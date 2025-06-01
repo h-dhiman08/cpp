ab467d8e31baabc19b4327360a796cd866d64781
Harsh Dhiman <harshdhiman7044@gmail.com>
2025-06-01 21:33:07 +0530

    Hard Problem

---
+++ DSA /array algorithms/buy_sell_stock.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<limits.h>
+using namespace std;
+
+void maxProfit(int *prices, int &size){
+    int bestBuy[100000];
+    bestBuy[0] = INT_MAX;
+}
+int main(){
+    int prices[] = {7,1,5,3,6,4};
+    int size = sizeof(prices)/sizeof(prices[0]);
+
+    return 0;
+}
