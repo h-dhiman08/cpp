d3d4b99d19f68b0e1ef18fadca3a5ef37485954a
Harsh Dhiman <harshdhiman7044@gmail.com>
2025-06-01 18:51:03 +0530

    Kdane Algorithm implemented

--- DSA /array algorithms/kdane_algorithm.cpp
+++ DSA /array algorithms/kdane_algorithm.cpp
@@ -6,10 +6,16 @@ using namespace std;
 
 int maxSub(const int *arr,const int &size){
     int maxSum = INT_MIN;
+    int currSum = 0;
     for(int i = 0; i < size; i++){
-
+        currSum += arr[i];
+        if (currSum < 0)
+        {
+            currSum = 0;
+        }
+        maxSum = max(maxSum, currSum);
     }
-    return 0;
+    return maxSum;
 }
 int main(){
     int arr[] = {2,-3,6,-5,4,2};
