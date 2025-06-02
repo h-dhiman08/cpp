3afec03244dcfb9f3f780ac8e8fb63acf7108479
Harsh Dhiman <harshdhiman7044@gmail.com>
2025-06-02 09:35:13 +0530

    Buy and Sell stock done

--- DSA /array algorithms/buy_sell_stock.cpp
+++ DSA /array algorithms/buy_sell_stock.cpp
@@ -2,13 +2,30 @@
 #include<limits.h>
 using namespace std;
 
-void maxProfit(int *prices, int &size){
+void maxProfit(const int *prices,const int &size){
     int bestBuy[100000];
     bestBuy[0] = INT_MAX;
+
+    // calculating the best buying Day for each selling day
+    for (int i = 1; i < size; i++)
+    {
+        bestBuy[i] = min(bestBuy[i-1],prices[i-1]);
+    }
+
+    // calculating the best profit
+    int maxProfit = 0; // required in case of decreasing graph
+    for (int i = 0; i < size; i++)
+    {
+        int currProfit = prices[i] - bestBuy[i];
+        maxProfit = max(maxProfit, currProfit);
+    }
+
+    cout << "Maximum possible profit: " << maxProfit << endl;
 }
 int main(){
     int prices[] = {7,1,5,3,6,4};
     int size = sizeof(prices)/sizeof(prices[0]);
 
+    maxProfit(prices,size);
     return 0;
 }
